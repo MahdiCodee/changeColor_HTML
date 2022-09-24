0ca42273494b2c519982792a2f28bc696ca4c210
MahdiCodee <113142913+MahdiCodee@users.noreply.github.com>
2022-09-24 20:39:00 +0330

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+const backGround = document.getElementById("background");
+
+function redColor() {
+    backGround.style.backgroundColor = "red";
+}
+
+function greenColor() {
+    backGround.style.backgroundColor = "green";
+}
+
+function blueColor() {
+    backGround.style.backgroundColor = "blue";
+}
+
+function yellowColor() {
+    backGround.style.backgroundColor = "yellow";
+}
+
+function itselfColor() {
+    backGround.style.backgroundColor = "#615F60";
+}
